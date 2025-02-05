ac176b4ccbb9e113ad194a96f2d12a2b8ea35eb0
Dimple Rana <dimpleranaji@gmail.com>
2025-02-05 11:47:02 +0530

    Database connected

--- lib/db.ts
+++ lib/db.ts
@@ -9,23 +9,32 @@ if (!MONGODB_URI) {
 
 let cached = global.mongoose;
 
-if(!cached){
-    cached = global.mongoose = {conn:null, promise: null}
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
 }
 
 
-export async function connectToDatabase(){
-    if(cached.conn){
+export async function connectToDatabase() {
+    if (cached.conn) {
         return cached.conn
     }
-    if(!cached.promise){
+    if (!cached.promise) {
         const opts = {
-            bufferCommands = true,
+            bufferCommands: true,
             maxPoolSize: 10
-            }
         }
+
+        cached.promise = mongoose
+            .connect(MONGODB_URI, opts).then(() =>
+                mongoose.connection)
     }
-    cached.promise = mongoose.connect()
-    .connect(MONGODP_URI, opts).then(()=>
-    mongoose.connection)
+
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        cached.promise = null
+        throw error
+    }
+    return cached.conn
 }
+
