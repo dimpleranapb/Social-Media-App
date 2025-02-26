4f818037fb41f70f58fe9673e9ab6b6b3cf6ad1a
Dimple Rana <dimpleranaji@gmail.com>
2025-02-26 23:41:56 +0530

    Api client created

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,3 +1,7 @@
+import { IVideo } from "@/models/Video";
+
+export type VideoFormData = Omit<IVideo, "_id">
+
 type fetchOptions = {
     method?: "GET" | "POST" | "PUT" | "DELETE";
     body?: any;
@@ -9,23 +13,44 @@ class ApiClient {
     private async fetch<T>(endpoint: string,
         options: fetchOptions = {}): Promise<T> {
 
-            const {method= "GET", body, headers= {}} = options
+        const { method = "GET", body, headers = {} } = options
 
-            const defaultHeaders = {
-                "Content-Type": "applicatiom/json",
-                ...headers
-            }
-            const response = await fetch(`/api${endpoint}`, {
-                method,
-                headers: defaultHeaders,
-                body: body? JSON.stringify(body): undefined
-            })
+        const defaultHeaders = {
+            "Content-Type": "applicatiom/json",
+            ...headers
+        }
+        const response = await fetch(`/api${endpoint}`, {
+            method,
+            headers: defaultHeaders,
+            body: body ? JSON.stringify(body) : undefined
+        })
 
-            if(!response.ok){
-                throw new Error(await response.text());
-            }
+        if (!response.ok) {
+            throw new Error(await response.text());
+        }
 
-            return response.json()
-    }   
+        return response.json()
+    }
+
+
+    async getVideos() {
+        return this.fetch<IVideo[]>("/videos")
+
+    }
+
+    async getAVideos(id: string) {
+        return this.fetch(`/videos/${id}`)
+    }
+
+    async createVideo(videoData: VideoFormData) {
+        return this.fetch("/videos", {
+            method: "POST",
+            body: videoData
+        })
+
+    }
+
 
 }
+
+export const apiClient = new ApiClient()
