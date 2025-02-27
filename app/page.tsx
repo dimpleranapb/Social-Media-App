7d015b208d63fecffc63c6aa26d3a258f6e7ff37
Dimple Rana <dimpleranaji@gmail.com>
2025-02-27 21:57:12 +0530

    page.tsx changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { apiClient } from "@/lib/api-client";
+import { IVideo } from "@/models/Video";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [videos, setVideos] = useState<IVideo[]>([]);
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const data = await apiClient.getVideos();
+        setVideos(data);
+      } catch (error) {}
+    };
+    fetchVideos();
+  }, []);
+
   return (
-<div>
-  <h1>Social Media App</h1>
-</div>
+    <div>
+      <h1>Social Media App</h1>
+    </div>
   );
 }
